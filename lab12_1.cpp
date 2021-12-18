46c6cd75a12687a09f566e53fb584ec790a32775
Reverse <jedsadakorn_k@cmu.ac.th>
2021-12-18 21:32:32 +0700

    completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,26 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+    int r;
+    string x;
+    srand(time(0));
+    r = rand()%9;
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i=0 ; i<3 ; i++){
+        cin.get();
+    }
+    if(r==0) x="A";
+    if(r==1) x="B+";
+    if(r==2) x="B";
+    if(r==3) x="C+";
+    if(r==4) x="C";
+    if(r==5) x="D+";
+    if(r==6) x="D";
+    if(r==7) x="F";
+    if(r==8) x="W";
+    cout << "You will get " << x << " in this 261102.";
+    return 0;
+}
